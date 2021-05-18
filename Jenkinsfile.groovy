0f52d3b6e5e9d66d47c5e6b4960dcd532d9d66a8
Ragin <ragin666@gmail.com>
2021-05-18 23:21:01 +0200

    Dev (#1)
    adding deployment features

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,18 +6,23 @@ pipeline {
         label 'pipeline-base-container'
     }
 
+    // For a single pipline job the branch name has to be resolved manually.
+    environment {
+        BRANCH_NAME = "${env.GIT_BRANCH.split('/').size() > 1 ? env.GIT_BRANCH.split('/')[1..-1].join('/') : env.GIT_BRANCH}"
+    }
+
     stages {
         stage('Deploy application') { steps { container(name: 'helm') { script {
             echo "Deploying application to ${env.BRANCH_NAME}..."
             echo "-> sync repositories..."
-            sh "helmfile repos"
+            sh "helmfile -e ${env.BRANCH_NAME} repos"
             echo "-> deploy charts..."
-            sh "helmfile -e ${env.BRANCH_NAME} apply"
+            sh "helmfile -e ${env.BRANCH_NAME} -n demo-${env.BRANCH_NAME} apply"
         } } } }
 
         stage('Deploy ingress router') { steps { container(name: 'helm') { script {
             echo "Deploying ingress to ${env.BRANCH_NAME}..."
-            sh "kubectl -n demo-${env.BRANCH_NAME} apply ingress.yaml"
+            sh "kubectl -n demo-${env.BRANCH_NAME} apply -f ingress.yaml"
         } } } }
     }
 }
--- Jenkinsfile.undeploy.groovy
+++ Jenkinsfile.undeploy.groovy
@@ -2,27 +2,25 @@ pipeline {
     options {
         buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
     }
+
     agent {
         label 'pipeline-base-container'
     }
 
     // For a single pipline job the branch name has to be resolved manually.
     environment {
-        def BRANCH_NAME = scm.branches[0].name
-        if (BRANCH_NAME.contains("*/")) {
-            BRANCH_NAME = BRANCH_NAME.split("\\*/")[1]
-        }
+        BRANCH_NAME = "${env.GIT_BRANCH.split('/').size() > 1 ? env.GIT_BRANCH.split('/')[1..-1].join('/') : env.GIT_BRANCH}"
     }
 
     stages {
         stage('Undeploy application') { steps { container(name: 'helm') { script {
             echo "Undeploying application from ${env.BRANCH_NAME}..."
-            sh "helmfile -e ${env.BRANCH_NAME} destroy"
+            sh "helmfile -e ${env.BRANCH_NAME} -n demo-${env.BRANCH_NAME} destroy"
         } } } }
 
         stage('Undeploy ingress router') { steps { container(name: 'helm') { script {
             echo "Undeploying ingress from ${env.BRANCH_NAME}..."
-            sh "kubectl -n demo-${env.BRANCH_NAME} delete ingress.yaml"
+            sh "kubectl -n demo-${env.BRANCH_NAME} delete -f ingress.yaml"
         } } } }
     }
 }
